8067fd28adca27a83880c61be196ec40ce8da1fe
YTRodi <yagorodi@gmail.com>
2021-03-18 21:34:38 -0300

    Ejercicio 01 completado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+/**
+ * Decorador '@Component': atributos de este componente
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mi-proyecto';
+  public edadUno = 0;
+  public edadDos = 0;
+  public resultadoSuma = 0;
+  public resultadoPromedio = 0;
+
+  calcularInputs(): any {
+    this.calcularSuma();
+    this.calcularPromedio();
+  }
+
+  calcularSuma(): any { this.resultadoSuma = this.edadUno + this.edadDos; }
+
+  calcularPromedio(): any { this.resultadoPromedio = this.resultadoSuma / 2; }
+
+  limpiarCampos(): any {
+    this.edadUno = 0;
+    this.edadDos = 0;
+    this.resultadoPromedio = 0;
+    this.resultadoSuma = 0;
+  }
 }
